a74c1619d54d38dfb30d701afd22ec0c7497f39a
TF2CutContent <404usernotfoundtf2@gmail.com>
2022-10-12 04:43:20 -0400

    Fixing out of bounds iteration
    Swore I had fixed this twice before.

--- localStorage.js
+++ localStorage.js
@@ -1,14 +1,14 @@
 let boxes = document.getElementsByClassName('box').length;
 
 function save() {	
-  for(let i = 1; i <= boxes; i++){
+  for(let i = 0; i <= boxes; i++){
 	  var checkbox = document.getElementById(String(i));
     localStorage.setItem("checkbox" + String(i), checkbox.checked);	
   }
 }
 
 //for loading
-for(let i = 1; i <= boxes; i++){
+for(let i = 0; i <= boxes; i++){
   if(localStorage.length > 0){
     var checked = JSON.parse(localStorage.getItem("checkbox" + String(i)));
     document.getElementById(String(i)).checked = checked;
