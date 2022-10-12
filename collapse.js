c02ba1ff31d779aaab5f5d0d8cc695a460c0f510
TF2CutContent <404usernotfoundtf2@gmail.com>
2022-10-12 01:04:52 -0400

    Update collapse.js

--- collapse.js
+++ collapse.js
@@ -4,17 +4,18 @@ var collapsible = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < collapsible.length; i++)
 {
-    collapsible[i].addEventListener("click", function()
-    {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block")
-        {
-            content.style.display = "none";
-        }
-        else
-        {
-            content.style.display = "block";
-        }
-    });
+	collapsible[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+
+		// experimental ternary change
+		content.style.display = (content.style.display === "block") ? "none" : "block";
+
+		// original code, if the ternary doesn't work
+		/* if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		} */
+	});
 }
