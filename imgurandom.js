135908a92765d9df52243f6bcbfe706b6e93761a
TF2CutContent <404usernotfoundtf2@gmail.com>
2022-10-12 02:53:18 -0400

    Update imgurandom.js

--- imgurandom.js
+++ imgurandom.js
@@ -13,6 +13,25 @@ jQuery(document).ready(function($) {
 	if (numImages.value > 200) numImages.value = 200;
 	else if (numImages.value < 1) numImages.value = 1;
 
+	let boxes = document.getElementsByClassName('box').length;
+
+	function save() {	
+		for(let i = 1; i <= boxes; i++) {
+			var checkbox = document.getElementById(String(i));
+			localStorage.setItem("checkbox" + String(i), checkbox.checked);	
+		}
+	}
+
+	//for loading
+	for (let i = 1; i <= boxes; i++) {
+		if (localStorage.length > 0) {
+			var checked = JSON.parse(localStorage.getItem("checkbox" + String(i)));
+			document.getElementById(String(i)).checked = checked;
+		}
+	}
+	
+	window.addEventListener('change', save);
+
 //      var prevImages = new Array();
 
 	var info_el = $("#info"),
