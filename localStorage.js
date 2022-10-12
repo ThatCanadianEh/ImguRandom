339eea9fadb48e2a8762ce767b06531788f43940
TF2CutContent <404usernotfoundtf2@gmail.com>
2022-10-12 08:50:35 -0400

    Update localStorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,10 +1,12 @@
 let boxes = document.getElementsByClassName('box').length;
 
 function save() {	
-  for (let i = 1; i <= boxes; i++) {
-	  var checkbox = document.getElementById(String(i));
-    localStorage.setItem("checkbox" + String(i), checkbox.checked);	
-  }
+	for (let i = 1; i <= boxes; i++) {
+		var checkbox = document.getElementById(String(i));
+		localStorage.setItem("checkbox" + String(i), checkbox.checked);	
+  	}
+	var numImages = document.getElementById("numImages");
+	localStorage.setItem("localNumImages", numImages.value);
 }
 
 window.addEventListener('change', save);
