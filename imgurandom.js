62d0de139ceca8480b1de91f985f8522d28f15a1
TF2CutContent <404usernotfoundtf2@gmail.com>
2022-10-12 03:41:38 -0400

    Update imgurandom.js

--- imgurandom.js
+++ imgurandom.js
@@ -201,4 +201,12 @@ jQuery(document).ready(function($) {
 		        document.getElementById(String(i)).checked = checked;
 	        }
         }
+        function save() {	
+                for (let i = 1; i <= boxes; i++) {
+	                var checkbox = document.getElementById(String(i));
+                        localStorage.setItem("checkbox" + String(i), checkbox.checked);	
+                }
+        }
+
+        window.addEventListener('change', save);
 });
